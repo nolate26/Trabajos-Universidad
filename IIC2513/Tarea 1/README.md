6a70f855d66ac6a6337ea18588ffdbd214057c7b
nolate26 <nicolas.olate@uc.cl>
2023-08-08 14:44:46 -0400

    agregando tarea 1 web

---
+++ IIC2513/Tarea 1/README.md
@@ -0,0 +1,10 @@
+# Tarea 1 - Sobre el contenido de la página
+
+### Objetivo
+
+Está página muestra y da información sobre quienes son para mí los mejores jugadores de fútbol hoy en día. En la cual salen algunas estadísticas, imágenes y clubes por los que ha pasado cada jugador. Además de resaltar todos los palmarés que tiene cada jugador. Cada jugador es una de las "fichas" que se piden.
+
+### Funcionalidades
+
+La página funciona con un index, que muestra el tema de la página con una navbar para poder recorrerla, esta tiene distintas funcionalidades aplicadas con css, ya que si la apreto me dirige a cada página y si apreto el ícono de pelota vuelvo al index. Si voy al contenido me encuentro con 5 botones los cuales al pulsarlos despliegan la información de cada jugador, esto lo implementé con *JavaScript*, desplegando la información de cada jugador y si apreto otro desaparece la info del anterior.
+Cree está página pues soy fanático del fútbol y de Cristiano Ronaldo por lo que me interesaba hacerlo de esto, ver imágenes de mis ídolos y reconocer su tremenda trayectoria.
---
+++ IIC2513/Tarea 1/src/assets/script.js
@@ -0,0 +1,176 @@
+function volver() {
+    window.location.href = '../../index.html'
+}
+
+function myFunction_1() {
+    var cr7 = document.getElementById("1");
+    var kaka = document.getElementById("2");
+    var neymar = document.getElementById("3");
+    var mbappe = document.getElementById("4");
+    var messi = document.getElementById("5");
+
+    kaka.setAttribute("hidden", "hidden");
+    neymar.setAttribute("hidden", "hidden");
+    mbappe.setAttribute("hidden", "hidden");
+    messi.setAttribute("hidden", "hidden");
+
+
+    if (cr7.hidden) {
+        cr7.removeAttribute("hidden");
+        button.innerText = "Hide div";
+    }
+    else {
+        cr7.setAttribute("hidden", "hidden");
+        button.innerText = "Show div";
+    }
+}
+function myFunction_2() {
+    var cr7 = document.getElementById("1");
+    var kaka = document.getElementById("2");
+    var neymar = document.getElementById("3");
+    var mbappe = document.getElementById("4");
+    var messi = document.getElementById("5");
+
+    cr7.setAttribute("hidden", "hidden");
+    neymar.setAttribute("hidden", "hidden");
+    mbappe.setAttribute("hidden", "hidden");
+    messi.setAttribute("hidden", "hidden");
+
+
+    if (kaka.hidden) {
+        kaka.removeAttribute("hidden");
+        button.innerText = "Hide div";
+    }
+    else {
+        kaka.setAttribute("hidden", "hidden");
+        button.innerText = "Show div";
+    }
+}
+function myFunction_3() {
+    var cr7 = document.getElementById("1");
+    var kaka = document.getElementById("2");
+    var neymar = document.getElementById("3");
+    var mbappe = document.getElementById("4");
+    var messi = document.getElementById("5");
+
+    kaka.setAttribute("hidden", "hidden");
+    cr7.setAttribute("hidden", "hidden");
+    mbappe.setAttribute("hidden", "hidden");
+    messi.setAttribute("hidden", "hidden");
+
+
+    if (neymar.hidden) {
+        neymar.removeAttribute("hidden");
+        button.innerText = "Hide div";
+    }
+    else {
+        neymar.setAttribute("hidden", "hidden");
+        button.innerText = "Show div";
+    }
+}
+function myFunction_4() {
+    var cr7 = document.getElementById("1");
+    var kaka = document.getElementById("2");
+    var neymar = document.getElementById("3");
+    var mbappe = document.getElementById("4");
+    var messi = document.getElementById("5");
+
+    kaka.setAttribute("hidden", "hidden");
+    neymar.setAttribute("hidden", "hidden");
+    cr7.setAttribute("hidden", "hidden");
+    messi.setAttribute("hidden", "hidden");
+
+
+    if (mbappe.hidden) {
+        mbappe.removeAttribute("hidden");
+        button.innerText = "Hide div";
+    }
+    else {
+        mbappe.setAttribute("hidden", "hidden");
+        button.innerText = "Show div";
+    }
+}
+function myFunction_5() {
+    var cr7 = document.getElementById("1");
+    var kaka = document.getElementById("2");
+    var neymar = document.getElementById("3");
+    var mbappe = document.getElementById("4");
+    var messi = document.getElementById("5");
+
+    kaka.setAttribute("hidden", "hidden");
+    neymar.setAttribute("hidden", "hidden");
+    mbappe.setAttribute("hidden", "hidden");
+    cr7.setAttribute("hidden", "hidden");
+
+
+    if (messi.hidden) {
+        messi.removeAttribute("hidden");
+        button.innerText = "Hide div";
+    }
+    else {
+        messi.setAttribute("hidden", "hidden");
+        button.innerText = "Show div";
+    }
+}
+//     if (kaka.hidden) {
+//         kaka.removeAttribute("hidden");
+//         button.innerText = "Hide div";
+//     }
+//     else {
+//         kaka.setAttribute("hidden", "hidden");
+//         button.innerText = "Show div";
+//     }
+//     if (neymar.hidden) {
+//         neymar.removeAttribute("hidden");
+//         button.innerText = "Hide div";
+//     }
+//     else {
+//         neymar.setAttribute("hidden", "hidden");
+//         button.innerText = "Show div";
+//     }
+//     if (mbappe.hidden) {
+//         mbappe.removeAttribute("hidden");
+//         button.innerText = "Hide div";
+//     }
+//     else {
+//         mbappe.setAttribute("hidden", "hidden");
+//         button.innerText = "Show div";
+//     }
+//     if (messi.hidden) {
+//         messi.removeAttribute("hidden");
+//         button.innerText = "Hide div";
+//     }
+//     else {
+//         messi.setAttribute("hidden", "hidden");
+//         button.innerText = "Show div";
+//     }
+
+// }
+
+//     if (cr7.style.display === "none") {
+//         cr7.style.display = "block";
+//     } else {
+//         cr7.style.display = "none";
+//     }
+//     if (kaka.style.display === "none") {
+//         kaka.style.display = "block";
+//     } else {
+//         kaka.style.display = "none";
+//     }
+//     if (neymar.style.display === "none") {
+//         neymar.style.display = "block";
+//     } else {
+//         neymar.style.display = "none";
+//     }
+//     if (mbappe.style.display === "none") {
+//         mbappe.style.display = "block";
+//     } else {
+//         mbappe.style.display = "none";
+//     }
+//     if (messi.style.display === "none") {
+//         messi.style.display = "block";
+//     } else {
+//         messi.style.display = "none";
+//     }
+
+// }
